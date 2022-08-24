1d64b5653f7162fd097174db83b15367bc2b21ad
vaibhav.bhatnagar2 <vaibhav.bhatnagar2@globallogic.com>
2022-08-24 06:17:14 +0530

    Add project files.

---
+++ TravelingDiaries/Migrations/20220824000512_COUPONS.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelingDiaries.Migrations
+{
+    public partial class COUPONS : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coupons",
+                columns: table => new
+                {
+                    CouponId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CouponCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    value = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coupons", x => x.CouponId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "CouponId", "CouponCode", "value" },
+                values: new object[,]
+                {
+                    { 1, "SAVE10", 10 },
+                    { 2, "SAVE70", 70 },
+                    { 3, "SUMMER50", 30 },
+                    { 4, "HAPPY40", 40 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coupons");
+        }
+    }
+}
